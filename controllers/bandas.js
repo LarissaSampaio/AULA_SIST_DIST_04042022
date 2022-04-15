5271d58ed0a87a4f2a35f70188af79476ac8f00b
Larissa Sampaio <larisampaio@hotmail.com>
2022-04-15 14:41:50 -0300

    V.02_15abr2022 - Banco Lowdb

--- controllers/bandas.js
+++ controllers/bandas.js
@@ -1,27 +1,35 @@
 import bandasDB from "../data/bandas.js";
+import { Low, JSONFile } from 'lowdb'
+
+const adapter = new JSONFile("./bancoTeste.json")
+const db = new Low(adapter)
+db.read()
 
 const controller = {
 
     showList: function(req, res) {
-        res.status(200).json(bandasDB);
+        res.status(200).json(db.data.bandas)
     },
     add: function(req,res){
         let {id, name, style} = req.body;
-        bandasDB.bandas.data[id] = {id, name, style}
+        db.data.bandas[id] = {id, name, style}
         //res.status(200).json({ status: "Ok"});
-        res.status(200).json(bandasDB);
+        db.write()
+        res.status(200).json(db.data.bandas)
     },
     update: function(req,res){
-        let {id, name, style} = req.body;
-        bandasDB.bandas.data[id] = {id, name, style}
+        let {id, name, style} = req.body
+        db.data.bandas[id] = {id, name, style}
+        db.write()
         //res.status(200).json({ status: "Ok"});
-        res.status(200).json(bandasDB);
+        res.status(200).json(db.data.bandas)
     },
     delete: function(req,res){
-        let id = req.body.id;
-        delete bandasDB.bandas.data[id];
+        let id = req.body.id
+        delete db.data.bandas[id]
+        db.write()
         //res.status(200).json({ status: "Ok"});
-        res.status(200).json(bandasDB);
+        res.status(200).json(db.data.bandas)
     }
 }
 
